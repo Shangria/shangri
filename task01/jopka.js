80ea2aec4d9359f9115ea64f14c54caf0996a371
shangria <shangrialena@gmail,com>
2019-06-30 21:15:20 +0300

    1

--- task01/jopka.js
+++
@@ -1,8 +0,0 @@
-function tiJopka(message) {
-    message = message || 'ты жопка?';
-    if (confirm(message)) {
-        return;
-    } else {
-        tiJopka('нет подтверди, что ты жопка');
-    }
-}
