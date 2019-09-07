a8c754244246f0cf72fbd51d1640f99e71992743
shangria <shangrialena@gmail,com>
2019-09-08 00:15:37 +0300

    1

--- wonderful places of ukraine/js/js.js
+++ wonderful places of ukraine/js/js.js
@@ -1,61 +1,130 @@
-let car = {
-    currentSpeed: 250,
-    previousSpeed: 0
-};
+/*
+var phrases = [
+    "Звучит неплохо",
+    "Да, это определенно надо сделать",
+    "Не думаю, что это хорошая идея",
+    "Может, не сегодня?",
+    "Компьютер говорит нет"
+];
 
-function gamnoed(){
-    car.previousSpeed = car.currentSpeed;
-    car.currentSpeed = 300;
-    console.log("Скорость стала: " + car.currentSpeed + " Скорость была: " + car.previousSpeed);
-}
+var phrases2 = [
+    "lalala",
+    "fdsfsd",
+    "gfgdfgfdgdfg",
+    "gfdgdf",
+    "ytrytry",
+    "6u776u76",
+    "hahahaha"
+];
 
-gamnoed();
+var phrases3 = [
+    "867",
+    "456",
+    "987"
+];
+var phrases4 = [
+    "pppppp"
+];
+
+// var arrays = [
+//     phrases,
+//     phrases2,
+//     phrases3,
+//     phrases4
+// ];
+
+// var arrays = [];
+// arrays[0] = phrases;
+// arrays[1] = phrases2;
+// arrays[2] = phrases3;
+// arrays[3] = phrases4;
 
 
-//
-// function toString(argument) {
-//     let result;
-//     if (typeof argument == 'string') {
-//         result = argument;
-//     } else if (typeof argument == 'number') {
-//         result = argument + 'xx';
-//     } else if (argument instanceof Array) {
-//         result = '';
-//         for (let i = 0; i < argument.length; i++) {
-//             result = result + ' ' + argument[i];
-//         }
-//     } else if (typeof argument == 'boolean') {
-//         if (argument) {
-//             result = 'da';
-//         } else {
-//             result = 'net';
-//         }
-//     } else {
-//         result = 'gamno'
-//     }
-//     return result;
-// }
-//
+var arrays = [];
+arrays.push(phrases);
+arrays.push(phrases2);
+arrays.push(phrases3);
+arrays.push(phrases4);
+arrays.push(phrases);
+arrays.push(phrases2);
+arrays.push(phrases3);
+arrays.push(phrases4);
+arrays.push(phrases);
+arrays.push(phrases2);
+arrays.push(phrases3);
+arrays.push(phrases4);
+arrays.push(phrases);
+arrays.push(phrases2);
+arrays.push(phrases3);
+arrays.push(phrases4);
+arrays.push(phrases);
+arrays.push(phrases2);
+arrays.push(phrases3);
+arrays.push(phrases4);
+arrays.push(phrases);
+arrays.push(phrases2);
+arrays.push(phrases3);
+arrays.push(phrases4);
+arrays.push(phrases);
+arrays.push(phrases2);
+arrays.push(phrases3);
+arrays.push(phrases4);
+arrays.push(phrases);
+arrays.push(phrases2);
+arrays.push(phrases3);
+arrays.push(phrases4);
 
-// function checkAge(age) {
-//     return age > 18 ? true : confirm('Родители разрешили?');
-// }
+logRandomStringForEachArray();
 
-function min(a,b) {
-    if (a < b) {
-        return a;
-    } else {
-        return b;
+
+function logRandomStringForEachArray() {
+    for (let i = 0; i < arrays.length; i++) { // выведет 0, затем 1, затем 2
+        console.log('------------------------------> i=' + i);
+        let array = arrays[i];
+        logRandomString(array);
     }
 }
 
 
-
-console.log(min(2, 5) == 2);
-console.log(min(3, -1) == -1);
-console.log(min(1, 1) == 1);
-
-
-function pow(a,b) {
-    return a^b;
+function generateIndex(array) {
+    var arraySize = array.length;
+    console.log('arraySize=' + arraySize);
+    let random = Math.random();
+    console.log('random=' + random);
+    let maxRandom = random * arraySize;
+    console.log('maxRandom=' + maxRandom);
+    let index = Math.floor(maxRandom);
+    console.log('index=' + index);
+    return index;
 }
+
+function logRandomString(array) {
+    var generatedIndex = generateIndex(array);
+    console.log('generatedIndex=' + generatedIndex);
+    let string = array[generatedIndex];
+    console.log('--------> tring=' + string);
+}
+
+
+
+*/
+
+/*let namesOfPeopleTeaser = ["Миша", "Алёна", "Арбузная голова"];
+let actionName = ["приятного апетита", "приятно побабить", "eхать на пупике"];
+let namesOfPeoplesTeaser = namesOfPeopleTeaser[Math.floor(Math.random() * 3)];
+let actionNames = actionName[Math.floor(Math.random() * 3)];
+let watermelonAffairs = namesOfPeoplesTeaser + " " + actionNames + "!!!";
+let watermelonAffairsInAction = "У " + watermelonAffairs + "будет хороший день" + " "+actionNames+".Всего хороего!!!";
+    console.log(watermelonAffairsInAction);*/
+
+function randomChar() {
+    let randomLetters = "gkvfdsgyuknbcaazsdfgn";
+    for (let myPassword = 0; myPassword.length < 10; myPassword++) {
+        myPassword = randomLetters[Math.floor(Math.random() * randomLetters.length)];
+        console.log('Мой пароль: ' + myPassword);
+    }
+}
+
+    for (var sheepCounted = 0; sheepCounted < 10; sheepCounted++) {
+        console.log("Посчитано овец: " + sheepCounted + "!");
+    }
