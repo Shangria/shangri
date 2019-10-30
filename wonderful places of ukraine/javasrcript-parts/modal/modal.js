46b506c9fb8a4cae92b35257e8201315807c7ec0
shangria <shangrialena@gmail,com>
2019-10-30 22:18:13 +0200

    gamno

---
+++ wonderful places of ukraine/javasrcript-parts/modal/modal.js
@@ -0,0 +1,27 @@
+$(function () {
+
+    let modal = $('#modal');
+    let closeModalButton = $('#closeModal');
+    let openModalButton = $('#openModal');
+
+
+    /*событие, которое запускается, когда страница полностью загрузилась, раньше было $(document).ready */
+
+    $(openModalButton).on('click', function () {
+        openModal();
+    });
+
+    $(closeModalButton).on('click', function () {
+        closeModal();
+    });
+
+
+    function openModal() {
+        $(modal).addClass('open');
+    }
+
+    function closeModal() {
+        $(modal).removeClass('open');
+    }
+
+});
